4928d679c4b78256c0ba4b70b38652b29bd781e6
Kenny Lau <kc_kennylau@yahoo.com.hk>
2025-07-08 02:51:03 +0100

    mk_all

--- EllipticCurve.lean
+++ EllipticCurve.lean
@@ -1,1 +1,7 @@
 import EllipticCurve.Basic
+import EllipticCurve.Grassmannians.Basic
+import EllipticCurve.Lemmas
+import EllipticCurve.ProjectiveSpace.Basic
+import EllipticCurve.ProjectiveSpace.TensorProduct.SymmetricMap
+import EllipticCurve.ProjectiveSpace.TensorProduct.SymmetricPower
+import EllipticCurve.Ring.Model
